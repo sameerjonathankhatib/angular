2dfba37d5d13ee49539fbd81147b8e620f7fc207
Sam <sameerjonathankhatib@gmail.com>
2025-03-24 11:44:48 +0800

    angualr progress

---
+++ essentials/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from "./components/user/user.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
