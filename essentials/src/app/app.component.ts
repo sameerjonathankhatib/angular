ca649ab351a3badc311a6de84245e1653bcb5430
Sam <sameerjonathankhatib@gmail.com>
2025-03-25 13:01:26 +0800

    session 2 ep 39

--- essentials/src/app/app.component.ts
+++ essentials/src/app/app.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from "./components/user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './components/tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id : string) {
+      this.selectedUserId = id;
+  }
 }
