ede582aec2f14ffb78659f941af4e2515a816e27
Sam <sameerjonathankhatib@gmail.com>
2025-03-26 11:47:14 +0800

    session 2 ep 48 done

--- essentials/src/app/components/user/user.component.ts
+++ essentials/src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, EventEmitter, input, Input, output, Output } from '@angular/core';
-
+import { User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,16 +8,15 @@ import { Component, computed, EventEmitter, input, Input, output, Output } from 
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) username!: string;
+  @Input({required: true}) user!:User;
+  @Input({required:true}) selected!: boolean;
   @Output() select = new EventEmitter();
   
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-   this.select.emit(this.id);
+   this.select.emit(this.user.id);
   }
 
 }
